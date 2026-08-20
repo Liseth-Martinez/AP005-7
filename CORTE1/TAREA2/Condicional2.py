3a4f87904951c30817a0a1604cc735b91fc48385
Dayanna Martinez <lidmartinezc@udistrital.edu.co>
2026-08-20 14:37:13 -0500

    Create Condicional2.py

---
+++ CORTE1/TAREA2/Condicional2.py
@@ -0,0 +1,9 @@
+precio = int(input("digite un numero "))
+
+if precio <= 1000:
+    print("Barato")
+elif precio > 1000 and precio <= 2000:
+    print("mediano") 
+elif precio > 2000 and precio <= 3000:
+else:
+    print("caro")
