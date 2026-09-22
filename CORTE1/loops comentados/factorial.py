290d5509b44f07bdcfe9483b023ee662900bd2c5
Dayanna Martinez <lidmartinezc@udistrital.edu.co>
2026-09-22 14:39:45 -0500

    Update factorial.py

--- CORTE1/loops comentados/factorial.py
+++ CORTE1/loops comentados/factorial.py
@@ -1,14 +1,12 @@
-# Repite el programa continuamente
-while True: 
-    value = int(input("Enter a positive integer value: "))  # Pide al usuario que ingrese un número entero positivo
-    print("Value: ", value)    # Muestra el valor ingresado
-    a = isinstance(value, int)  # Verifica si el valor es un número entero
-    if a == True and value > 0:   # Comprueba que sea entero y mayor que cero
-        fact = 1  # Inicializa el factorial en 1
-        for i in range (1, value + 1): # Recorre los números desde 1 hasta el valor ingresado
-            fact = fact*i # Multiplica el factorial por cada número            
-        print(f'The factorial of {value} is: ', fact)  # Muestra el factorial calculado
-    
-    # Si el valor no es positivo, muestra un mensaje de error
-    else: 
-        print("Please, enter a positive integer number") 
+while True:                            # Repite el programa continuamente
+ 
+    value = int(input("Enter a positive integer value: "))  # Pide un número entero positivo
+    print("Value: ", value)             # Muestra el valor ingresado
+    a = isinstance(value, int)          # Verifica si el valor es entero
+    if a == True and value > 0:         # Comprueba que sea entero y positivo
+        fact = 1                        # Inicializa el factorial en 1
+        for i in range (1, value + 1):  # Recorre los números desde 1 hasta el valor
+            fact = fact*i               # Multiplica los números para calcular el factorial
+        print(f'The factorial of {value} is: ', fact)  # Muestra el factorial
+    else:                               # Si el valor no es positivo
+        print("Please, enter a positive integer number")  # Muestra un mensaje de error
