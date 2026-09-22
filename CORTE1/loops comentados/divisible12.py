d3fa7d93520b2cf32639308d376f6974be267bab
Dayanna Martinez <lidmartinezc@udistrital.edu.co>
2026-09-22 14:38:47 -0500

    Update divisible12.py

--- CORTE1/loops comentados/divisible12.py
+++ CORTE1/loops comentados/divisible12.py
@@ -1,4 +1,5 @@
-for i in range(100, 301):  # Recorre los números desde 100 hasta 300 
-    if (i%12) != 0:  # Verifica si el número no es divisible entre 12
-        continue  # Salta al siguiente número 
-    print(i)   # Muestra los números que sí son divisibles entre 12
+for i in range(100, 301):              # Recorre los números del 100 al 300
+    if (i%12) != 0:                    # Verifica si el número no es divisible entre 12
+        continue                        # Salta al siguiente número
+    print(i)                            # Muestra el número divisible entre 12
+
