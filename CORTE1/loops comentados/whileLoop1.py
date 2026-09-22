ba8b833a705826eceae9cb0b1d49072df6a22fcf
Dayanna Martinez <lidmartinezc@udistrital.edu.co>
2026-09-22 14:51:03 -0500

    Create whileLoop1.py

---
+++ CORTE1/loops comentados/whileLoop1.py
@@ -0,0 +1,5 @@
+for i in range(1,6):          # Recorre los números del 1 al 5
+    while i <= 4:             # Repite mientras i sea menor o igual a 4
+        i += 1                # Aumenta el valor de i en 1
+        print(i)              # Muestra el valor actual de i
+    break                     # Termina el ciclo for
